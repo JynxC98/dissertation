6619d2c36009181b03fe40fe98129bb388a24485
JynxC98 <parikh.harsh.hp@gmail.com>
2023-06-07 14:26:15 +0100

    worked on tweaking the code for consistency and implemented the functions for sharpe and sortino ratio

--- codes/portfolio_allocation/portfolio_allocation.py
+++ codes/portfolio_allocation/portfolio_allocation.py
@@ -2,17 +2,55 @@
 Script that would select the stocks which would outperform the market returns.
 We will be using the NSE market for our stock selection.
 """
-from typing import List, Type
+from typing import List
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 import yfinance as yf
 
+NUM_TRADING_DAYS = 252  # Assumption
 RISK_FREE_RATE = (
-    0.073  # Data provided by https://tradingeconomics.com/india/government-bond-yield
-)
+    0.073
+    / NUM_TRADING_DAYS  # Data provided by https://tradingeconomics.com/india/government-bond-yield
+)  # Assuming risk free rate to be constant, we calculate daily risk free return
+
+
+# The function outside the `StockSelection` class are used to feed the stock data.
+def calculate_sharpe_and_sortino_ratio(
+    returns: np.array, risk_free_rate: float
+) -> List:
+    """
+    Function used to calculate the Sharpe and Sortino Ratio for the stocks. \\
+    Sharpe ratio is given as (E[X] - rf)/std(excess returns) \\
+    Sortino ratio is given as (E[X] - rd)/ std(negative asset returns)
+
+    Inputs
+    ------
+    returns: Log returns of the daily stock data
+    risk_free_rate: Tresury bond rates, assumed to be constant throughout the time period.
+
+    Returns
+    -------
+    Sharpe ratio and Sortino ratio respectively
+
+    """
+
+    portfolio_return = returns.mean() * 252
+    excess_return = portfolio_return - risk_free_rate * 252
+
+    # Calculation of Sharpe Ratio
+    portfolio_volatility = np.std(excess_return)
+    sharpe_ratio = (excess_return) / portfolio_volatility
+
+    # Calculation of Sortino Ratio
+    downside_returns = np.where(returns < risk_free_rate, returns - risk_free_rate, 0)
+    downside_std = np.std(downside_returns)
+    sortino_ratio = excess_return / downside_std
+
+    return [sharpe_ratio, sortino_ratio]
 
 
 class StockSelection:
@@ -33,9 +71,7 @@ class StockSelection:
 
     """
 
-    def __init__(
-        self, tickers: List, start_date: Type[datetime], end_date: Type[datetime]
-    ) -> None:
+    def __init__(self, tickers: List, start_date: datetime, end_date: datetime) -> None:
         """
         Initialisation function of the `StockSelection` class.
 
@@ -46,24 +82,31 @@ class StockSelection:
         self.end_date = end_date
 
         # These parameters will be stored after calculation
+        self.portfolio_data = None
+        self.returns = None
 
-    def get_data_from_yahoo(self) -> Type[pd.DataFrame]:
+    def get_data_from_yahoo(self) -> pd.DataFrame:
         """
+        The function generates the historical data of daily stock returns.
 
         Returns
         -------
-        Historical data comprising of
-        1. Opening price
-        2. High
-        3. Low
-        4. Closing price
-        5. Volume
-        6. Dividends
-
+        Closing price of the historical data
         """
         stock_data = {}
 
         for stock in self.tickers:
             ticker = yf.Ticker(stock)
-            stock_data[stock] = ticker.history(start=self.start_date, end=self.end_date)
+            stock_data[stock] = ticker.history(
+                start=self.start_date, end=self.end_date
+            )["Close"]
         return pd.DataFrame(stock_data)
+
+    def calculate_returns(self) -> np.array:
+        """
+        Calculates the log return of the data.
+        """
+        data = self.get_data_from_yahoo()
+        log_return = np.log(data / data.shift(1))
+        self.returns = log_return[1:]  # We save the value of log returns.
+        return log_return[1:]  # We skip the first row to eliminate the NaN values.
